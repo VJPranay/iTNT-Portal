64aacda8ae8687192df7583b273304ec2f38f335
VJ Pranay <pranaymadasi1@gmail.com>
2024-02-07 17:09:11 +0530

    latest

--- ldevcatalyst/registrations/industry/views.py
+++ ldevcatalyst/registrations/industry/views.py
@@ -4,6 +4,14 @@ from ..models import IndustryRegistrations
 from datarepo.models import AreaOfInterest,State,IndustryCategory
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+import random
+import string
+from django.http import JsonResponse
+from profiles.models import User
+from django.db.utils import IntegrityError
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 
@@ -31,7 +39,7 @@ def industry_registrations(request,registraion_status=None):
 @login_required
 def approve_registration(request):
     if request.method == 'POST':
-        print(request.POST)
+
         registration_id = request.POST.get('registration_id',None)
         if not registration_id:
             return JsonResponse({'success': False, 'error': 'Missing registration ID'}, status=400)
@@ -40,8 +48,48 @@ def approve_registration(request):
                 registration = IndustryRegistrations.objects.get(id=registration_id)
                 registration.status = 'approved'
                 registration.save()
+                
+                # Generate username from registration ID
+                username = registration.registration_id
+                print(username)
+
+                # Generate random 6-digit number
+                password = ''.join(random.choices(string.digits, k=6))
+                print(password)
+
+                # Create user with the generated username and random password
+                try:
+                    user = User.objects.create_user(username=username, password=password)
+                    user.is_active = True
+                    user.save()
+                except IntegrityError:
+                    user = User.objects.get(username=username)
+                    user.delete()
+                    user = User.objects.create_user(username=username, password=password)
+                    user.is_active = True
+                    user.save()
+                print(user.username)
+                email_host = 'mail.ldev.in'
+                email_port = 465
+                email_username = 'itntadmin@ldev.in'
+                email_password = 'Pranay123@'
+                subject = 'You iTNT registration has been approved'
+                body = f'''
+                        Username: {user.username}
+                        Password: {password}
+                        Login URL: https://ldev.in
+                        '''
+                message = MIMEMultipart()
+                message['From'] = email_username
+                message['To'] = registration.email  # Add the additional email address
+                message['Subject'] = subject
+                message.attach(MIMEText(body, 'plain'))
+                with smtplib.SMTP_SSL(email_host, email_port) as server:
+                    print(server.login(email_username, email_password))
+                    print(server.sendmail(email_username, [registration.email], message.as_string()))
                 return JsonResponse({'success': True})
             except IndustryRegistrations.DoesNotExist:
+                print("error")
                 return JsonResponse({'success': False, 'error': 'Registration not found'}, status=404)
     else:
         return JsonResponse({'success': False, 'error': 'Method not allowed'}, status=405)
