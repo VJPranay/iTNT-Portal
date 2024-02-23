af6caeea09c20179d4a87e1cd14e31bc8143b6ac
VJ Pranay <pranaymadasi1@gmail.com>
2024-02-24 03:28:01 +0530

    production

--- ldevcatalyst/ldevcatalyst/settings.py
+++ ldevcatalyst/ldevcatalyst/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-68bn2*xnujz@*i2(xb61^kk3vhzo8sfyw3mrn7***srqkm49_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = [
 'itntportal.ldev.in',
 'itnthub.tn.gov.in',
