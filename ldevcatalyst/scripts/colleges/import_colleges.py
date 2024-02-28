119b3367f7e7bb45412bec51a622e2bc3e0738e9
vj <vj@macOS.local>
2024-02-28 16:39:19 +0530

    data import scripts

---
+++ ldevcatalyst/scripts/colleges/import_colleges.py
@@ -0,0 +1,23 @@
+import csv
+import os
+from ldevcatalyst.settings import BASE_DIR
+from datarepo.models import Institution,District
+from django.db import IntegrityError
+
+def add_institution_from_csv(csv_file_path):
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            district_name = row[0]
+            college_name = row[2]
+            try:
+                district_info = District.objects.get(name=district_name)
+                try:
+                    new_institution = Institution.objects.create(district_id = district_info.id,name=college_name)
+                    new_institution.save()
+                except IntegrityError as e:
+                    print(e)
+            except District.DoesNotExist:
+                print(f"{district_name} doesnt exist in DB")
+csv_file_path = os.path.join(BASE_DIR, "scripts/colleges/colleges.csv")
+add_institution_from_csv(csv_file_path)
--- ldevcatalyst/scripts/departments/import_departments.py
+++ ldevcatalyst/scripts/departments/import_departments.py
@@ -11,6 +11,5 @@ def add_departments_from_csv(csv_file_path):
             department = Department.objects.create(name=department_name)
             department.save()
             print(row[0])
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 csv_file_path = os.path.join(BASE_DIR, "scripts/departments/departments.csv")
 add_departments_from_csv(csv_file_path)
