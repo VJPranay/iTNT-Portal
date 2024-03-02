1562546cd2b9510648572f0aaabcde4a6590860e
VJ Pranay <pranaymadasi1@gmail.com>
2024-03-02 15:59:55 +0530

    suport admin

--- ldevcatalyst/support/admin.py
+++ ldevcatalyst/support/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import SupportRequest
-# Register your models here.
-admin.site.register(SupportRequest)
+
+@admin.register(SupportRequest)
+class SupportRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'mobile', 'account_role', 'support_category', 'created_at')
+    search_fields = ('name', 'email', 'mobile', 'account_role', 'support_category', 'short_description')
+    list_filter = ('account_role', 'support_category', 'created_at')
+    ordering = ('-created_at',)
