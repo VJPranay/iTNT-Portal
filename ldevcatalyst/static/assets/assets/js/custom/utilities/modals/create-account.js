eb8fe772dddc5c8190270f7a4c067be2d57c648f
VJ Pranay <pranaymadasi1@gmail.com>
2024-03-02 15:58:00 +0530

    fix css

---
+++ ldevcatalyst/static/assets/assets/js/custom/utilities/modals/create-account.js
@@ -0,0 +1,284 @@
+"use strict";
+
+
+// Class definition
+var KTCreateAccount = function () {
+	// Elements
+	var modal;	
+	var modalEl;
+
+	var stepper;
+	var form;
+	var formSubmitButton;
+	var formContinueButton;
+
+
+	// Variables
+	var stepperObj;
+	var validations = [];
+
+	// Private Functions
+	var initStepper = function () {
+		// Initialize Stepper
+		stepperObj = new KTStepper(stepper);
+
+		// Stepper change event
+		stepperObj.on('kt.stepper.changed', function (stepper) {
+			if (stepperObj.getCurrentStepIndex() === 2) {
+				formSubmitButton.classList.remove('d-none');
+				formSubmitButton.classList.add('d-inline-block');
+				formContinueButton.classList.add('d-none');
+			} else if (stepperObj.getCurrentStepIndex() === 3) {
+				formSubmitButton.classList.add('d-none');
+				formContinueButton.classList.add('d-none');
+			} else {
+				formSubmitButton.classList.remove('d-inline-block');
+				formSubmitButton.classList.remove('d-none');
+				formContinueButton.classList.remove('d-none');
+			}
+		});
+
+		// Validation before going to next page
+		stepperObj.on('kt.stepper.next', function (stepper) {
+			console.log('stepper.next');
+
+			// Validate form before change stepper step
+			var validator = validations[stepper.getCurrentStepIndex() - 1]; // get validator for currnt step
+
+			if (validator) {
+				validator.validate().then(function (status) {
+					console.log('validated!');
+
+					if (status == 'Valid') {
+						stepper.goNext();
+
+						KTUtil.scrollTop();
+					} else {
+						Swal.fire({
+							text: "Sorry, looks like there are some errors detected, please try again.",
+							icon: "error",
+							buttonsStyling: false,
+							confirmButtonText: "Ok, got it!",
+							customClass: {
+								confirmButton: "btn btn-light"
+							}
+						}).then(function () {
+							KTUtil.scrollTop();
+						});
+					}
+				});
+			} else {
+				stepper.goNext();
+
+				KTUtil.scrollTop();
+			}
+		});
+
+		// Prev event
+		stepperObj.on('kt.stepper.previous', function (stepper) {
+			console.log('stepper.previous');
+
+			stepper.goPrevious();
+			KTUtil.scrollTop();
+		});
+	}
+
+	var handleForm = function() {
+		formSubmitButton.addEventListener('click', function (e) {
+			// Validate form before change stepper step
+			var validator = validations[1]; // get validator for last form
+
+			validator.validate().then(function (status) {
+				console.log('validated!');
+
+				if (status == 'Valid') {
+					// Prevent default button action
+					e.preventDefault();
+
+					// Disable button to avoid multiple click 
+					formSubmitButton.disabled = true;
+                    
+
+					// Show loading indication
+					formSubmitButton.setAttribute('data-kt-indicator', 'on');
+
+					// Simulate form submission
+					setTimeout(function() {
+						// Hide loading indication
+						formSubmitButton.removeAttribute('data-kt-indicator');
+
+						// Enable button
+						formSubmitButton.disabled = false;
+
+						stepperObj.goNext();
+					}, 2000);
+				} else {
+					Swal.fire({
+						text: "Sorry, looks like there are some errors detected, please try again.",
+						icon: "error",
+						buttonsStyling: false,
+						confirmButtonText: "Ok, got it!",
+						customClass: {
+							confirmButton: "btn btn-light"
+						}
+					}).then(function () {
+						KTUtil.scrollTop();
+					});
+				}
+			});
+		});
+
+	}
+
+	var initValidation = function () {
+		// Step 1
+		validations.push(FormValidation.formValidation(
+			form,
+			{
+				fields: {
+					company_name: {
+						validators: {
+							notEmpty: {
+								message: 'Please enter company name'
+							}
+						}
+					},
+                    industry_type: {
+						validators: {
+							notEmpty: {
+								message: 'Please select your industry type'
+							}
+						}
+					},
+                    location_state: {
+						validators: {
+							notEmpty: {
+								message: 'Please select your state'
+							}
+						}
+					},
+                    location_district: {
+						validators: {
+							notEmpty: {
+								message: 'Please select your district'
+							}
+						}
+					},
+                    collaboration_sector: {
+						validators: {
+							notEmpty: {
+								message: 'Area of collaboration is required'
+							}
+						}
+					}
+				},
+				plugins: {
+					trigger: new FormValidation.plugins.Trigger(),
+					bootstrap: new FormValidation.plugins.Bootstrap5({
+						rowSelector: '.fv-row',
+                        eleInvalidClass: '',
+                        eleValidClass: ''
+					})
+				}
+			}
+		));
+
+		// Step 2
+		validations.push(FormValidation.formValidation(
+			form,
+			{
+				fields: {
+                    poc_name: {
+						validators: {
+							notEmpty: {
+								message: 'Point of contact name is required'
+							}
+						}
+					},
+                    poc_email: {
+						validators: {
+							notEmpty: {
+								message: 'Point of contact email is required'
+							},
+                            emailAddress: {
+                                message: 'Invalid email address',
+                            },
+						}
+					},
+                    poc_mobile: {
+						validators: {
+							notEmpty: {
+								message: 'Point of contact mobile number is required'
+							},
+                            phone: {
+                                country: function () {
+                                    return 'IN';
+                                },
+                                message: 'Invalid phone number',
+                            },
+						}
+					},
+				},
+				plugins: {
+					trigger: new FormValidation.plugins.Trigger(),
+					// Bootstrap Framework Integration
+					bootstrap: new FormValidation.plugins.Bootstrap5({
+						rowSelector: '.fv-row',
+                        eleInvalidClass: '',
+                        eleValidClass: ''
+					})
+				}
+			}
+		));
+
+		// Step 3
+		validations.push(FormValidation.formValidation(
+			form,
+			{
+				fields: {
+					
+				},
+				plugins: {
+					trigger: new FormValidation.plugins.Trigger(),
+					// Bootstrap Framework Integration
+					bootstrap: new FormValidation.plugins.Bootstrap5({
+						rowSelector: '.fv-row',
+                        eleInvalidClass: '',
+                        eleValidClass: ''
+					})
+				}
+			}
+		));
+	}
+
+	return {
+		// Public Functions
+		init: function () {
+			// Elements
+			modalEl = document.querySelector('#kt_modal_create_account');
+
+			if ( modalEl ) {
+				modal = new bootstrap.Modal(modalEl);	
+			}					
+
+			stepper = document.querySelector('#kt_create_account_stepper');
+
+			if ( !stepper ) {
+				return;
+			}
+
+			form = stepper.querySelector('#kt_create_account_form');
+			formSubmitButton = stepper.querySelector('[data-kt-stepper-action="submit"]');
+			formContinueButton = stepper.querySelector('[data-kt-stepper-action="next"]');
+
+			initStepper();
+			initValidation();
+			handleForm();
+		}
+	};
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTCreateAccount.init();
+});
