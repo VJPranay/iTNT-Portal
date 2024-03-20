401368a5e7199b64daedad442e3e8719c1f824fd
VJ Pranay <pranaymadasi1@gmail.com>
2024-03-20 19:24:12 +0530

    changes

--- ldevcatalyst/reports/sme/views.py
+++ ldevcatalyst/reports/sme/views.py
@@ -1,18 +1,32 @@
-from django.db.models import Count
+from django.db.models import Count, Case, When, Value, CharField
 from django.shortcuts import render
-from registrations.models import ResearcherRegistrations, AreaOfInterest, District, Institution
+from registrations.models import ResearcherRegistrations, AreaOfInterest, PatentInfo, District, Institution
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def researcher_overview(request):
-    researcher_count_by_interest = ResearcherRegistrations.objects.filter(area_of_interest__isnull=False).values('area_of_interest__name').annotate(researcher_count=Count('id')).filter(researcher_count__gt=0).order_by('-researcher_count')
+    # Get approved area of interest categories and their counts
+    approved_categories = AreaOfInterest.get_approved_categories()
+    researcher_count_by_interest = ResearcherRegistrations.objects.filter(area_of_interest__in=approved_categories).values('area_of_interest__name').annotate(researcher_count=Count('id')).order_by('-researcher_count')
+
+    # Get counts for other area of interest categories
+    other_interest_count = ResearcherRegistrations.objects.exclude(area_of_interest__in=approved_categories).aggregate(count=Count('id'))['count'] or 0
+
+    # Combine approved categories and count into a list
     by_area_of_interest = []
     for item in researcher_count_by_interest:
         by_area_of_interest.append({ 
             'area_of_interest__name': item['area_of_interest__name'],
             'researcher_count': item['researcher_count'], 
         })
+    
+    # Add "Others" category with its count to the list
+    by_area_of_interest.append({
+        'area_of_interest__name': 'Others',
+        'researcher_count': other_interest_count,
+    })
 
+    # Researcher count by district
     researcher_count_by_district = ResearcherRegistrations.objects.filter(district__isnull=False).values('district__name').annotate(researcher_count=Count('id')).filter(researcher_count__gt=0).order_by('-researcher_count')
     by_district = []
     for item in researcher_count_by_district:
@@ -21,13 +35,20 @@ def researcher_overview(request):
             'researcher_count': item['researcher_count'],
         })
 
-    researcher_count_by_institution = Institution.objects.filter(researcherregistrations__isnull=False).values('name').annotate(researcher_count=Count('researcherregistrations')).filter(researcher_count__gt=0).order_by('-researcher_count')
-    by_institution = []
-    for item in researcher_count_by_institution:
-        by_institution.append({
-            'institution__name': item['name'],
-            'researcher_count': item['researcher_count'],
+    # Researcher count by institution
+    researcher_count_by_institution_above_20 = Institution.objects.annotate(researcher_count=Count('researcherregistrations')).filter(researcher_count__gt=20).order_by('-researcher_count')
+    institutions_above_20 = []
+    for item in researcher_count_by_institution_above_20:
+        institutions_above_20.append({
+            'institution__name': item.name,
+            'researcher_count': item.researcher_count,
         })
+
+    other_institution_count = Institution.objects.annotate(researcher_count=Count('researcherregistrations')).filter(researcher_count__lte=20).aggregate(total_count=Count('id'))
+    other_institutions = [{
+        'institution__name': 'Other Institutions',
+        'researcher_count': other_institution_count['total_count'],
+    }]
 
     counts = {
         'researcher_count': ResearcherRegistrations.objects.all().count(),
@@ -36,5 +57,6 @@ def researcher_overview(request):
         'counts': counts,
         'area_of_interest_data': by_area_of_interest,
         'district_data': by_district,
-        'institution_data': by_institution,
+        'institutions_above_20': institutions_above_20,
+        'other_institutions': other_institutions,
     })
