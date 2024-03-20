e0d8e78bc2947fed0bcd341be168c1bd82637e1f
VJ Pranay <pranaymadasi1@gmail.com>
2024-03-20 19:33:47 +0530

    test

--- ldevcatalyst/reports/sme/views.py
+++ ldevcatalyst/reports/sme/views.py
@@ -12,7 +12,7 @@ def researcher_overview(request):
     # Get counts for other area of interest categories
     other_interest_count = ResearcherRegistrations.objects.exclude(area_of_interest__in=approved_categories).aggregate(count=Count('id'))['count'] or 0
 
-    # Combine approved categories and count into a list
+    # Combine approved categories and count into a list for area of interest
     by_area_of_interest = []
     for item in researcher_count_by_interest:
         by_area_of_interest.append({ 
@@ -20,13 +20,13 @@ def researcher_overview(request):
             'researcher_count': item['researcher_count'], 
         })
     
-    # Add "Others" category with its count to the list
+    # Add "Others" category with its count to the list for area of interest
     by_area_of_interest.append({
         'area_of_interest__name': 'Others',
         'researcher_count': other_interest_count,
     })
 
-    # Researcher count by district
+    # Get counts for districts with researcher counts greater than 0
     researcher_count_by_district = ResearcherRegistrations.objects.filter(district__isnull=False).values('district__name').annotate(researcher_count=Count('id')).filter(researcher_count__gt=0).order_by('-researcher_count')
     by_district = []
     for item in researcher_count_by_district:
@@ -35,20 +35,29 @@ def researcher_overview(request):
             'researcher_count': item['researcher_count'],
         })
 
-    # Researcher count by institution
-    researcher_count_by_institution_above_20 = Institution.objects.annotate(researcher_count=Count('researcherregistrations')).filter(researcher_count__gt=20).order_by('-researcher_count')
-    institutions_above_20 = []
-    for item in researcher_count_by_institution_above_20:
-        institutions_above_20.append({
-            'institution__name': item.name,
-            'researcher_count': item.researcher_count,
-        })
+    # Get counts for institutions with researcher counts above 20
+    institutions_above_20 = Institution.objects.annotate(
+        researcher_count=Count('researcherregistrations')
+    ).filter(researcher_count__gt=20).values('name', 'researcher_count').order_by('-researcher_count')
 
-    other_institution_count = Institution.objects.annotate(researcher_count=Count('researcherregistrations')).filter(researcher_count__lte=20).aggregate(total_count=Count('id'))
-    other_institutions = [{
+    # Get count for other institutions
+    other_institutions_count = Institution.objects.annotate(
+        researcher_count=Count('researcherregistrations')
+    ).filter(researcher_count__lte=20).aggregate(count=Count('id'))['count'] or 0
+
+    # Combine institutions above 20 and count into a list for institutions
+    by_institution = []
+    for item in institutions_above_20:
+        by_institution.append({
+            'institution__name': item['name'],
+            'researcher_count': item['researcher_count'],
+        })
+    
+    # Add "Other Institutions" category with its count to the list for institutions
+    by_institution.append({
         'institution__name': 'Other Institutions',
-        'researcher_count': other_institution_count['total_count'],
-    }]
+        'researcher_count': other_institutions_count,
+    })
 
     counts = {
         'researcher_count': ResearcherRegistrations.objects.all().count(),
@@ -57,6 +66,5 @@ def researcher_overview(request):
         'counts': counts,
         'area_of_interest_data': by_area_of_interest,
         'district_data': by_district,
-        'institutions_above_20': institutions_above_20,
-        'other_institutions': other_institutions,
+        'institution_data': by_institution,
     })
