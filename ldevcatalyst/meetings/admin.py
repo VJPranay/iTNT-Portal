ebeccf8c867efbfe726fcf1aa950ee7bfe2403e7
VJ Pranay <pranaymadasi1@gmail.com>
2024-05-20 09:35:24 +0530

    changes

--- ldevcatalyst/meetings/admin.py
+++ ldevcatalyst/meetings/admin.py
@@ -5,4 +5,3 @@ from .models import MeetingRequests
 class MeetingRequestsAdmin(admin.ModelAdmin):
     list_display = ('start_up', 'vc', 'message', 'created', 'updated', 'status')
     list_filter = ('status',)
-    search_fields = ('start_up__name', 'vc__firm_name', 'status')
