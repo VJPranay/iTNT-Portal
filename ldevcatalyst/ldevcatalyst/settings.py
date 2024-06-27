a5c96368cd25e840990a8cc96706567d7576e36d
VJ Pranay <pranaymadasi1@gmail.com>
2024-06-27 15:31:52 +0530

    changes

--- ldevcatalyst/ldevcatalyst/settings.py
+++ ldevcatalyst/ldevcatalyst/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-68bn2*xnujz@*i2(xb61^kk3vhzo8sfyw3mrn7***srqkm49_*
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-DEV = False 
+DEV = True 
 
 ALLOWED_HOSTS = [
 'itnthub.tn.gov.in',
